3aeb3aef4526ee4be353daefee48ed1c21bf9e43
lldan <safinildan@gmail.com>
2023-09-24 23:56:19 +0300

    добавил флаг FLAG_DEV

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './scss/main.scss';
+const FLAG_DEV = true;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <App FLAG_DEV={FLAG_DEV} />
   </React.StrictMode>,
 );
