92aeb6d952aa9cdca3d1a2c2a10797d36a4fe1d3
lldan <safinildan@gmail.com>
2023-09-25 20:50:05 +0300

    почти дописал StartDesktop

--- src/components/universal/Switch/Switch.tsx
+++ src/components/universal/Switch/Switch.tsx
@@ -14,13 +14,13 @@ type switchPropsType = {
 export default function Switch({ func, def = false }: switchPropsType) {
   const [isHandled, setIsHandled] = React.useState(def);
   const handleClick = () => {
+    func(!isHandled);
     setIsHandled(!isHandled);
-    func(isHandled);
   };
   return (
-    <div className={isHandled ? 'switch' : 'switch active'} onClick={handleClick}>
+    <div className={isHandled ? 'switch active' : 'switch '} onClick={handleClick}>
       <motion.div
-        className={isHandled ? 'switch__circle' : 'switch__circle active'}
+        className={isHandled ? 'switch__circle active' : 'switch__circle'}
         layout
         transition={spring}
       ></motion.div>
