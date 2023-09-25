03118ac786013a49bc9c908189c0debc13245dc6
lldan <safinildan@gmail.com>
2023-09-25 13:07:04 +0300

    селект

--- src/components/modal/content/ModalAccountGroup.tsx
+++ src/components/modal/content/ModalAccountGroup.tsx
@@ -1,16 +1,13 @@
-import classes from "../Modal.module.scss";
-import Select from "../../universal/Select/Select.tsx";
+import classes from '../Modal.module.scss';
+import Select from '../../universal/Select/Select.tsx';
 
 export default function ModalAccountGroup() {
   return (
     <div>
-      <div className={classes["modalContent__group"]}>
-        <span className={classes["account-title"]}>Группа</span>
-        <span className={classes["select__wrapper"]}>
-          <Select
-            list={["11-201", "11-202", "11-203", "11-204", "11-205"]}
-            def={1}
-          />
+      <div className={classes['modalContent__group']}>
+        <span className={classes['account-title']}>Группа</span>
+        <span className={classes['select__wrapper']}>
+          <Select list={['11-201', '11-202', '11-203', '11-204', '11-205']} />
         </span>
       </div>
     </div>
