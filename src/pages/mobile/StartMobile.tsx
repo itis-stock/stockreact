6dc2b54896c11ee79802db821f31f789c56efcba
lldan <safinildan@gmail.com>
2023-09-26 16:48:44 +0300

    фикс startmobile

--- src/pages/mobile/StartMobile.tsx
+++ src/pages/mobile/StartMobile.tsx
@@ -1,10 +1,12 @@
-// import StartPage from "../../components/mobile/StartPage/StartPage.tsx";
-import classes from "./StartMobile.module.scss";
-import MainMobile from "./Main/MainMobile.tsx";
+import StartPage from '../../components/mobile/StartPage/StartPage.tsx';
+import classes from './StartMobile.module.scss';
+// import MainMobile from './Main/MainMobile.tsx';
 
 export default function StartMobile() {
-  return <div className={classes["mobile__wrapper"]}>
-    {/*<StartPage/>*/}
-    <MainMobile/>
-  </div>;
+  return (
+    <div className={classes['mobile__wrapper']}>
+      <StartPage />
+      {/* <MainMobile /> */}
+    </div>
+  );
 }
