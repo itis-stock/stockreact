b90ace89b7647497715cc7e5e0826457060551d5
lldan <safinildan@gmail.com>
2023-09-28 04:45:10 +0300

    как мне одним предложением обозначить изменение 23 файлов?

--- src/desktop/StartDesktop.tsx
+++ src/desktop/StartDesktop.tsx
@@ -1,5 +1,59 @@
 import React from 'react';
-
+import './scss/StartDesktop.scss';
+import logo from '/desktop/logo.png';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 export default function StartDesktop() {
-  return <div>StartDesktop</div>;
+  return (
+    <div className="start">
+      <div className="start__wrapper">
+        <img src={logo} alt="logo" className="start__logo" />
+        <div className="start__content">
+          <div className="start__tip start__tip-what">Что?</div>
+          <div className="start__tip start__tip-why">Зачем?</div>
+          <div className="start__content-item">
+            Хз, что тут объяснять. Твердо и четко. Новый сток
+          </div>
+          <div className="start__content-item">Просто потому что могу и мне похуй в принципе</div>
+          <div className="start__content-item">
+            <span>Ты можешь разработать буквально все, что ты захочешь на основе моего API</span>
+            <span>Хочешь разработать android или ios приложение - вперед</span>
+            <span>Хочешь попрактиковаться Java или C# - перепиши мое API на этих языках</span>
+            <span>Хочешь разработать сайт на любой технологии - вперед</span>
+            <span>Это чистый воды практика для любого</span>
+          </div>
+        </div>
+        <div className="start__socials">
+          <a href="https://github.com/itis-stock" className="start__socials-item">
+            Github
+          </a>
+          <a href="https://t.me/lld4n" className="start__socials-item">
+            Telegram разработчика
+          </a>
+          <div className="start__tip start__tip-github">
+            Создавай свои проекты на основе stockAPI
+          </div>
+          <div className="start__tip start__tip-telegram">24 на 7, 7 дней в неделю онлайн</div>
+        </div>
+        <motion.a
+          href="https://oauth.vk.com/authorize?client_id=51757279&redirect_uri=http://localhost:5173/check&display=page&scope=groups&response_type=token&v=5.131"
+          className="start__vk"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <span className="start__white">Продолжить с VK</span>
+          <span className="start__light">Только для тех, кто состоит в оригинальном стоке</span>
+        </motion.a>
+      </div>
+
+      <div className="start__bottom">
+        <Link to="/history" className="start__link">
+          1.0.0b
+        </Link>
+        <Link to="/API" className="start__link">
+          API
+        </Link>
+      </div>
+    </div>
+  );
 }
---
+++ src/desktop/components/NonFunctional.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import '../scss/NonFunctional.scss';
+import logo from '/desktop/logo.png';
+import { Link } from 'react-router-dom';
+
+type NonFunctionalPropsType = {
+  err: boolean;
+};
+const texts = [
+  'ты попал на ошибку, сочувствую',
+  'yarn create vite@latest',
+  'завариваем чай',
+  'устанавливаем git',
+];
+export default function NonFunctional({ err }: NonFunctionalPropsType) {
+  const [selectedIndex, setSelectedIndex] = React.useState(
+    Math.floor(Math.random() * (texts.length - 1)) + 1,
+  );
+
+  React.useEffect(() => {
+    if (err) {
+      setSelectedIndex(0);
+    } else {
+      setInterval(() => {
+        const ind = Math.floor(Math.random() * (texts.length - 1)) + 1;
+        setSelectedIndex(ind);
+      }, 1000);
+    }
+  }, []);
+  return (
+    <div className="non-functional">
+      <div className="non-functional__wrapper">
+        <img src={logo} alt="logo" className="non-functional__logo" />
+        <div className="non-functional__text">{texts[selectedIndex]}...</div>
+        {err ? (
+          <div className="non-functional__light">
+            Если вы находитесь на этой странице более минуты, то это значит что что-то пошло не так.
+            Рекомендуется вернуться на домашнюю страницу и попробовать еще раз
+          </div>
+        ) : (
+          <div className="non-functional__light">
+            Попробуйте снова. Если ошибка не пропадет напишите разработчику
+          </div>
+        )}
+
+        {err ? (
+          <Link to="/" className="non-functional__error">
+            Вернуться домой
+          </Link>
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
+  );
+}
